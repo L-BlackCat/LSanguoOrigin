fb4b96f6cba970a4e05bc54278c326ade14eb0e0
Li <819088740@qq.com>
2022-11-03 14:31:25 +0800

    init

---
+++ src/java/com/lc/sgo/ServerBoot.java
@@ -0,0 +1,9 @@
+package com.lc.sgo;
+
+/**
+ * @author Chunlin Li
+ * @date 2022/11/2
+ * @desc
+ */
+public class ServerBoot {
+}
---
+++ src/java/com/lc/sgo/hero/bean/HeroBean.java
@@ -0,0 +1,10 @@
+package com.lc.sgo.hero.bean;
+
+/**
+ * @author Chunlin Li
+ * @date 2022/11/2
+ * @desc
+ */
+public class HeroBean {
+
+}
